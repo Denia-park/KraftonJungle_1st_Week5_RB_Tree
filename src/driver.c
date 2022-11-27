0e7789d6b22e56c2c385a7bcc3c2fa949988d6b8
HyunggiPark <phg9587@naver.com>
2022-11-27 23:50:44 +0900

    test(driver): 테스트 코드 추가

--- src/driver.c
+++ src/driver.c
@@ -1,5 +1,10 @@
 #include "rbtree.h"
+#include <assert.h>
+#include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    rbtree* p_rbt = new_rbtree();
+    assert(p_rbt != NULL);
 
+    printf("Test Complete\n");
 }
